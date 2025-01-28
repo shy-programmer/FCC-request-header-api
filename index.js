906b3feb7ce22acd1a234fe2357232c9998b65d5
Shy programmer <abdulazeezarowolo@gmail.com>
2025-01-28 21:10:41 +0000

    Everything

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get('/api/whoami', (req, res) => {
+  //{"ipaddress":"159.20.14.100",
+  // "language":"en-US,en;q=0.5",
+  // "software":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0"}
+  res.json({
+    ipaddress: req.socket.remoteAddress,
+    language: req. acceptsLanguages()[0],
+    software: req.get('User-Agent')
+  })
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
